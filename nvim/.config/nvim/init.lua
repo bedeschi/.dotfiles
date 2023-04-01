8157dfc509a0fa5a0759e09f0a24cefa05dfbe2a
Caio Bedeschi <caio@Caios-MacBook-Pro.local>
2023-04-01 19:28:03 -0300

    feat: Updating nvim and tmux

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,1 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.treesitter"
-require "user.lsp"
+require("caio")
---
+++ nvim/.config/nvim/lua/caio/init.lua
@@ -0,0 +1,3 @@
+require("caio.remap")
+require("caio.set")
+require("caio.packer")
